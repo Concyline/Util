ae7392d9addaa24258606449c73d2945efc75d74
AUREO\concy <concyinfo@gmail.com>
2025-12-11 11:34:23 -0300

    Add novos metodos STR

--- util/src/main/java/br/com/util/Str.java
+++ util/src/main/java/br/com/util/Str.java
@@ -1,21 +1,36 @@
 package br.com.util;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Str {
 
-    public static String Format(String value, Object... parms) throws Exception{
+    public static String format(String template, Object... values) {
+        for (int i = 0; i < values.length; i++) {
+            template = template.replace("{" + i + "}", values[i].toString());
+        }
+        return template;
+    }
 
-        for (int i = 0; i < parms.length; i++) {
+    public static Double arredondaNumero(Double numeto, int casas) {
+        BigDecimal bd = new BigDecimal(numeto);
+        bd = bd.setScale(casas, RoundingMode.HALF_UP);
+        double arredondado = bd.doubleValue();
+        return arredondado;
+    }
 
-            String param = String.valueOf(parms[i]);
-            String tag = String.format("{%s}",i);
+    public static String join(Object... valores) {
+        StringBuilder sb = new StringBuilder();
 
-            if(value.contains(tag)) {
-                value = value.replace(tag, param);
+        for (int i = 0; i < valores.length; i++) {
+            sb.append(String.valueOf(valores[i]));
+
+            if (i < valores.length - 1) {
+                sb.append(" ");
             }
-
         }
 
-        return value;
+        return sb.toString();
     }
 
 }
